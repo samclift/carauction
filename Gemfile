5fe9a84967a20ad049d19cc29d22de7d73939f38
Sam Clift <sam@thinking.co.nz>
2013-11-03 17:15:02 +1300

    make index,pages and install bootstrap

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
 gem 'rails', '4.0.1'
+gem 'bootstrap-sass'
 
 group :doc do
   gem 'sdoc', require: false
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Carauction::Application.routes.draw do
+  get "pages/home"
+  get 'about' => 'pages#about'
+
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
